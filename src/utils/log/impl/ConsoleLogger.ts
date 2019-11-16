d3da07d675d5780c153d7da44c721d802a8524b2
Lukáš Novák <novalu@novalu.info>
2019-11-16 23:45:34 +0100

    Remove extra logging from ConsoleLogger.

--- src/utils/log/impl/ConsoleLogger.ts
+++ src/utils/log/impl/ConsoleLogger.ts
@@ -5,45 +5,28 @@ import { Logger } from "../Logger";
 
 @injectable()
 class ConsoleLogger implements Logger {
-
-  private pe: PrettyError;
-
-  private logExtra(op: (extra) => void, extra: any) {
-    if (extra instanceof Error) {
-      op(this.pe.render(extra))
-    } else {
-      op(extra);
-    }
-  }
-
   public debug(body: string, extra?: any) {
     console.log(body);
-    if (extra) this.logExtra((extraParam) => console.log(extraParam), extra);
   }
 
   public error(body: string, extra?: any) {
     console.log(body);
-    if (extra) this.logExtra((extraParam) => console.log(extraParam), extra);
   }
 
   public info(body: string, extra?: any) {
     console.log(body);
-    if (extra) this.logExtra((extraParam) => console.log(extraParam), extra);
   }
 
   public trace(body: string, extra?: any) {
     console.log(body);
-    if (extra) this.logExtra((extraParam) => console.log(extraParam), extra);
   }
 
   public warn(body: string, extra?: any) {
     console.log(body);
-    if (extra) this.logExtra((extraParam) => console.log(extraParam), extra);
   }
 
   public fatal(body: string, extra?: any) {
     console.log(body);
-    if (extra) this.logExtra((extraParam) => console.log(extraParam), extra);
   }
 }
 
